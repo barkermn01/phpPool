ac0e0ae09afa338aae571f572597419a73c3774b
Martin Barker <welfordmartin@gmail.com>
2013-04-02 02:47:46 +0100

    Support for multi-hostnames, added support for different hostnames to act as different services, added these options to the config.json so they can be set mine_host and site_host

--- index.php
+++ index.php
@@ -13,5 +13,16 @@ $serverControl = new ServerControl($conf);
 $serverControl->setDatabaseConnection($db);
 
 $serverControl->checkLogin();
-$serverControl->handleProxy();
+if($_SERVER['HTTP_HOST'] == $conf->pool->mine_host){
+	$serverControl->handleProxy();
+}elseif($_SERVER['HTTP_HOST'] == $conf->pool->site_host){
+	?>
+		<h1>Under Construction</h1>
+		<p>
+			Currently phpPool does not have a working Web Interface <br />
+			Check out our repo on GitHub <a href="https://github.com/barkermn01/phpPool">https://github.com/barkermn01/phpPool</a><br />
+			or you can donate to help support this project: 19rzPNQ9SScBxRZgu5rttmNEtBnpt9iEKT
+		</p>
+	<?php
+}
 ?>
